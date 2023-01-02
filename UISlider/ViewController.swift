d75397c87b204ecd49eabea254bf34f409626bb2
Aleksandr Karpov <papadoster13@gmail.com>
2023-01-03 00:53:59 +0100

    clean

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -11,30 +11,16 @@ import AVFoundation
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var sliderVolume: UISlider!
-    
-    @IBOutlet weak var sliderDuration: UISlider!
-    
     var player = AVAudioPlayer()
     var timer = Timer()
-//    let slider = UISlider()
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    @IBOutlet weak var sliderDuration: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         self.sliderDuration.setValue(Float(player.currentTime), animated: true)
-        
-        
-        // Slider
-//        self.slider.frame = CGRect(x: 0, y: 0, width: 300, height: 23)
-//        self.slider.center = self.view.center
-//        self.slider.minimumValue = 0.0
-//        self.slider.maximumValue = 100.0
-//        self.view.addSubview(slider)
-        
-        // Add Target
-//        self.slider.addTarget(self, action: #selector(changeSlider), for: .valueChanged)
         
         do {
             if let audioPath = Bundle.main.path(forResource: "audio", ofType: "mp3") {
@@ -45,22 +31,11 @@ class ViewController: UIViewController {
             print("ERROR")
         }
         
-        print("\(self.sliderDuration.maximumValue)")
-        print("\(self.sliderDuration.value)")
-        self.sliderDuration.setValue(Float(player.currentTime), animated: true)
-        print("\(self.sliderDuration.value)")
-//        self.player.play()
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateSliderDuration), userInfo: nil, repeats: true)
-        
         updateSliderDuration()
     }
     
-//    @objc func changeSlider(sender: UISlider) {
-//        if sender == slider {
-//            self.player.currentTime = TimeInterval(sender.value)
-//        }
-//    }
-
+    
     @IBAction func playButtonPressed(_ sender: UIButton) {
         self.player.play()
     }
